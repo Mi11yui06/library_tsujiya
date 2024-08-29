167e30bf66ef91ea127d061f08fbc5d753737fc4
Miyui TSUJIYA <tsujiya.miyui.1106@gmail.com>
2024-08-29 14:34:34 +0900

    複数貸出業務の細かな修正を行いました

--- app/controllers/counters_controller.rb
+++ app/controllers/counters_controller.rb
@@ -6,6 +6,8 @@ class CountersController < ApplicationController
       @return_loans = @member.loans.where(return_date: nil)
       @unreturned_count = @return_loans.count
       @loan = Loan.new
+      @loan_date = params[:loan_date] || Date.today
+      @stock_ids = params[:stock_ids] || []
     else
       flash[:danger] = "会員が見つかりません。"
       redirect_to root_path
@@ -50,7 +52,6 @@ class CountersController < ApplicationController
     end
     
     stock_ids.each do |stock_id|
-      next if @errors.key?(stock_id) 
       stock = Stock.find_by(id: stock_id)
       if stock.nil?
         @errors[stock_id] = "存在しない資料IDです。"
@@ -60,9 +61,6 @@ class CountersController < ApplicationController
         @errors[stock_id] = "この資料は貸出中です。"
       end
     end
-
-    logger.debug "Errors: #{@errors.inspect}"
-    logger.debug "Error Messages: #{@error_messages.inspect}"
   
     if @errors.any? || @error_messages.any?
       render :new, status: :unprocessable_entity
@@ -72,7 +70,6 @@ class CountersController < ApplicationController
         loan.set_due_date # 返却期日を設定
         loan
       end
-      @stock_ids = stock_ids
       render :confirm_loan
     end
   end
